19b28f2d687d1bd73af35f40544bf89f15f5863f
sam-fsd <kabirusam037@gmail.com>
2023-08-14 22:18:37 -0700

    removed to_dict method on obj

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,7 +16,7 @@ class FileStorage:
 
     def all(self):
         """returns the dictionary __objects"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """ sets in __objects the obj with
@@ -25,18 +25,19 @@ class FileStorage:
         obj_id = obj.id
         obj_classname = type(obj).__name__
         obj_key = f"{obj_classname}.{obj_id}"
-        self.__objects[obj_key] = obj.to_dict()
+        FileStorage.__objects[obj_key] = obj
 
     def save(self):
         """serializes __objects to the JSON file"""
-        with open(self.__file_path, 'a', encoding='utf-8') as f:
-            json.dump(self.__objects, f)
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
+            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            json.dump(d, f)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
-        if len(self.__file_path) != 0:
+        if len(FileStorage.__file_path) != 0:
             try:
-                with open(self.__file_path, encoding='utf-8') as f:
-                    self.__objects = json.load(f)
+                with open(FileStorage.__file_path, encoding='utf-8') as f:
+                    FileStorage.__objects = json.load(f)
             except:
                 pass
